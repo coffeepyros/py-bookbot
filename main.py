36c16cd7713e6ed92e537cb8624c6962da427d98
coffeepyros <coffeepyros@gmail.com>
2026-05-07 18:55:06 +0200

    zed formating

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from stats import num_of_words, num_of_chars
+from stats import num_of_chars, num_of_words
 
-def get_book_text(path) :
+
+def get_book_text(path):
     with open(path) as file:
         file_content = file.read()
         return file_content
 
-def main() :
+
+def main():
     file_path = "books/frankenstein.txt"
     book_text = get_book_text(file_path)
     # print(book_text)
     print("Found " + str(num_of_words(book_text)) + " total words")
     print(num_of_chars(book_text))
-main()
 
+
+main()
